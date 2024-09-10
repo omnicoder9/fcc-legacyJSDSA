8650988535723557159d92ac83d01714af3c0165
omnicoder9 <nknapp53928@gmail.com>
2024-09-10 15:17:48 -0400

    basics part 1

---
+++ basics.js
@@ -0,0 +1,29 @@
+console.log("please donate to freeCodeCamp")
+
+//In computer science, data is anything that is meaningful to the computer. 
+//JavaScript provides eight different data types which are undefined, null, boolean, string, symbol, bigint, number, and object.
+
+//variable names can be made up of numbers, letters, $, _, but may not contain spaces or start with a number
+//string literal/string: a series of zero or more characters enclosed in single or double quotes
+
+/* 
+When JavaScript variables are declared, they have an initial value of undefined. 
+If you do a mathematical operation on an undefined variable your result will be NaN which means "Not a Number". 
+If you concatenate a string with an undefined variable, you will get a string of undefined.
+*/
+
+//best practice: Write variable names in JavaScript in camelCase. In camelCase, multi-word variable names have the first word in lowercase and the first letter of each subsequent word is capitalized.
+
+//One of the biggest problems with declaring variables with the var keyword is that you can easily overwrite variable declarations.
+//A keyword called let was introduced in ES6, a major update to JavaScript, to solve this potential issue with the var keyword. You'll learn about other ES6 features in later challenges.
+//So unlike var, when you use let, a variable with the same name can only be declared once.
+
+//const has all the awesome features that let has, with the added bonus that variables declared using const are read-only. They are a constant value, which means that once a variable is assigned with const, it cannot be reassigned
+
+//Note: It is common for developers to use uppercase variable identifiers for immutable values and lowercase or camelCase for mutable values (objects and arrays).
+
+//The remainder operator % gives the remainder of the division of two numbers.
+//Note: The remainder operator is sometimes incorrectly referred to as the modulus operator. It is very similar to modulus, but does not work properly with negative numbers.
+
+//compound assignment with augmented division
+
